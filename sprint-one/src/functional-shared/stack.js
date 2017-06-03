4e905d6392e66d4e91caf4747410067dd620445e
Abiy Melaku <agirma08@gmail.com>
2017-06-03 14:48:42 -0700

    Restart for Review

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,30 +1,9 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  
-  var someInstance = {};
-  _(someInstance).extend(stackMethods);
-  
-  someInstance._storage = {};
-  someInstance._size = 0;
-  
-  return someInstance;
 };
-
 
 var stackMethods = {};
 
-stackMethods.push = function(value) {
-  this._storage[this._size] = value;
-  this._size++; 
-}
 
-stackMethods.pop = function() {
-  if ( this._size > 0 ) { this._size--; }
-  var result = this._storage[this._size];
-  return result;
-}
-
-stackMethods.size = function() {
-  return this._size;
-}
+Contact GitHub API Training Shop Blog About
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,24 +3,15 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  
-  //pointer to track size
-  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    storage[size] = value;
-    size++;
   };
 
   someInstance.pop = function() {
-    size && size--;
-    var result = storage[size];
-    return result;
   };
 
   someInstance.size = function() {
-    return size;
   };
 
   return someInstance;
