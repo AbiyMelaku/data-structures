e41ce1f8d8c13d4914055769170746f2357ebc69
Abiy Melaku <agirma08@gmail.com>
2017-06-03 16:59:13 -0700

    Redo all instatiation patterns

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,7 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  
+  someInstance._storage = {};
+  someInstance._first = 0;
+  someInstance._last = 0;
+  
+  _(someInstance).extend(queueMethods);
+  
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this._storage[this._last] = value;
+  this._last++;
+}
+
+queueMethods.dequeue = function() {
+  var result = this._storage[this._first];
+  if ( this._last > this._first ) { this._first++; }
+  return result;
+}
+
+queueMethods.size = function() {
+  return this._last - this._first;
+}
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  
+  //to track the front of stack
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if ( size > 0 ) { size--; }
+    var result = storage[size];
+    return result;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
