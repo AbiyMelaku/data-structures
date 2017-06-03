e4a49cc2868e078cbb36591d0ab6c7e4ea0f4cbf
Abiy Melaku <agirma08@gmail.com>
2017-06-02 23:08:30 -0700

    Prototypal instantiation pattern Queue class

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  
+  someInstance._storage = {};
+  someInstance._first = 0;
+  someInstance._last = 0;
+  
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this._storage[this._last] = value;
+  this._last++;
+}
+
+queueMethods.dequeue = function() {
+  var result = this._storage[this._first];
+  if ( this._last > this._first) { this._first++; }
+  return result;
+}
+
+queueMethods.size = function() {
+  return this._last - this._first;
+}
 
 
